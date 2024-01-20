c61e17e29d9fa4292d746246627edc005310c949
raccoonjeong <rana3782@gmail.com>
2024-01-21 08:37:25 +0900

    23강 초반

--- src/main/java/com/cos/jwtserver/config/SecurityConfig.java
+++ src/main/java/com/cos/jwtserver/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.cos.jwtserver.config;
 
+import com.cos.jwtserver.filter.MyFilter1;
+import com.cos.jwtserver.filter.MyFilter3;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
@@ -20,12 +24,21 @@ public class SecurityConfig {
     private final CorsFilter corsFilter;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // BasicAuthenticationFilter가 동작하기전에 MyFilter3이 동작한다.
+        // After든 Before든 직접 만든 Filter보다는 똑같이 먼저 동작함
+        // 스프링 시큐리티보다 먼저 동작하게 하려면 Before로 걸어라
+//        http.addFilterBefore(new MyFilter3(), BasicAuthenticationFilter.class);
+
+        // 스프링 시큐리티 이전에 가장 빨리 동작하는 필터가 된다.
+        http.addFilterBefore(new MyFilter3(), SecurityContextPersistenceFilter.class);
         http.csrf(CsrfConfigurer::disable);
 
         // 세션을 사용하지 않겠다.
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.addFilter(corsFilter); //@CrossOrigin(인증X), 시큐리티 필터에 등록 인증O
+        // 폼태그로 로그인을 안하겠다.
         http.formLogin(form -> form.disable());
+
         http.httpBasic(httpBasic -> httpBasic.disable());
         http.authorizeHttpRequests(auth ->
                 auth.requestMatchers("/api/v1/user/**")
---
+++ src/main/java/com/cos/jwtserver/filter/MyFilter1.java
@@ -0,0 +1,28 @@
+package com.cos.jwtserver.filter;
+
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MyFilter1 implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request,
+                         ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        String headerAuth = req.getHeader("Authorization");
+        System.out.println(headerAuth);
+        
+        System.out.println("필터1");
+        chain.doFilter(request, response);
+
+    }
+}
